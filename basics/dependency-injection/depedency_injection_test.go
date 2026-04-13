31f6c6bb5e86ad0c61838b30a9c8c4bb89d6d32f
Zach Wilson <zachthewilson@gmail.com>
2026-04-13 11:56:12 -0700

    dependency injection lesson complete

---
+++ basics/dependency-injection/depedency_injection_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	t.Run("test greet", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+		Greet(&buffer, "Chris")
+
+		got := buffer.String()
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+}
